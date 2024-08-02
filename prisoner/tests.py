12fa7f78939ad160a3f056cb18e7b247eff68277
Lior Bakalo <103962173+LiorBak@users.noreply.github.com>
2024-08-02 14:50:14 +0300

    Update tests.py

--- prisoner/tests.py
+++ prisoner/tests.py
@@ -6,7 +6,7 @@ from . import InformedConsentPage, Introduction, Decision, ResultsWaitPage, Resu
 class PlayerBot(Bot):
     def play_round(self):
         if self.player.round_number == 1:
-            yield InformedConsentPage, dict(Email="xyz@abc.123")
+            yield InformedConsentPage
             yield Introduction
         yield Decision, dict(cooperate=True if random.randint(0, 1) == 1 else False)
         yield Results
