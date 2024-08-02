b183d6cc7003580ee2f51e18a4ec6e849bfa3448
Lior Bakalo <103962173+LiorBak@users.noreply.github.com>
2024-08-02 15:31:48 +0300

    Update __init__.py add bonus to payoff

--- survey/__init__.py
+++ survey/__init__.py
@@ -85,7 +85,7 @@ class Payment_info(Page):
     @staticmethod
     def vars_for_template(player: Player):
         participant = player.participant
-        player.payoff = -participant.total_score  # so that participant.payoff_plus_participation_fee() will function properly
+        player.payoff = participant.bonus - participant.total_score  # so that participant.payoff_plus_participation_fee() will function properly
         return dict(
             fwd_url=get_fwd_url(
                 participant.payoff_plus_participation_fee(),
